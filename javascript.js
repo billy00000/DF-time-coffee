547f89ff109f9d2b95e1c2e6b554314cfb1b4ff3
billy00000 <billy801208@gmail.com>
2019-08-03 19:44:05 +0800

    20190803修正

--- javascript.js
+++ javascript.js
@@ -103,6 +103,48 @@ $(document).ready(function(){
           }
         ]
       });
-    }
-)
 
+      // 元素 捲動 (匿名函式() {  });
+    $(window).scroll(function() {
+        
+        if ($(window).scrollTop() > 200) {  // 如果 螢幕頂端 > 200
+
+            $(".btn-top").fadeIn(500);       // 顯示按鈕
+
+        } else {                            // 否則
+
+            $(".btn-top").fadeOut(500);      // 隱藏按鈕
+
+        }
+    }); 
+}); 
+
+var texts = ["找回", "尋找", "激發"];    // 打字 - 陣列
+var speed = 250;                                        // 打字速度 - 毫秒
+var index = 0;                                          // 索引值 - 從 0 開始
+var classname = ".type";                                // 類別名稱
+var count = 0;                                          // 文字數量
+var wait = 8;                                           // 等待
+
+// 函式 打字
+function type() {
+
+    var t = document.querySelector(classname);          // 搜尋元素
+
+    var letter = texts[index].slice(0, count++);        // 切割文字(開始位置，切割數量)
+
+    t.innerHTML = letter;                               // 插入 HTML = 文字
+
+    if (count == texts[index].length + wait) {          // 文字數量 等於 當前文字長度+等待
+        index++;                                        // 索引值 +1
+        count = 0;                                      // 文字數量 歸零
+    }
+
+    if (index == texts.length) {                        // 如果索引值等於長度
+        index = 0;                                      // 索引值 歸零
+    }
+
+    setTimeout(type, speed);                            // 重複執行方法指定間隔時間
+}
+
+document.addEventListener("DOMContentLoded", type());   // DOMContentLoded 網頁載入完成後執行
